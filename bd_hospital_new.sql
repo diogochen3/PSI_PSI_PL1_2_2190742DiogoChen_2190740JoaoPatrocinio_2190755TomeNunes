aae5d7f5fbcaca7be14d9136ff6e379e4a52c122
tome nunes <2190755@my.ipleiria.pt>
2020-11-16 17:58:33 +0000

    base de dados

---
+++ bd_hospital_new.sql
@@ -0,0 +1,337 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 16-Nov-2020 às 17:57
+-- Versão do servidor: 10.4.10-MariaDB
+-- versão do PHP: 7.0.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bd_hospital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `auth_assignment`
+--
+
+DROP TABLE IF EXISTS `auth_assignment`;
+CREATE TABLE IF NOT EXISTS `auth_assignment` (
+  `item_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  PRIMARY KEY (`item_name`,`user_id`),
+  KEY `idx-auth_assignment-user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `auth_assignment`
+--
+
+INSERT INTO `auth_assignment` (`item_name`, `user_id`, `created_at`) VALUES
+('admin', '1', 1605539068),
+('funcionario', '3', 1605539068),
+('medico', '2', 1605539068),
+('utente', '4', 1605539068);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `auth_item`
+--
+
+DROP TABLE IF EXISTS `auth_item`;
+CREATE TABLE IF NOT EXISTS `auth_item` (
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `type` smallint(6) NOT NULL,
+  `description` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `rule_name` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `data` blob DEFAULT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  `updated_at` int(11) DEFAULT NULL,
+  PRIMARY KEY (`name`),
+  KEY `rule_name` (`rule_name`),
+  KEY `idx-auth_item-type` (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `auth_item`
+--
+
+INSERT INTO `auth_item` (`name`, `type`, `description`, `rule_name`, `data`, `created_at`, `updated_at`) VALUES
+('admin', 1, NULL, NULL, NULL, 1605539068, 1605539068),
+('createMarcacao', 2, 'Criar marcação', NULL, NULL, 1605539068, 1605539068),
+('CreateMedico', 2, 'Criar Medico', NULL, NULL, 1605539068, 1605539068),
+('CreateReceita', 2, 'Criar receita', NULL, NULL, 1605539068, 1605539068),
+('DeleteMarcacao', 2, 'Eliminar marcação', NULL, NULL, 1605539068, 1605539068),
+('DeleteMedico', 2, 'Eliminar Medico', NULL, NULL, 1605539068, 1605539068),
+('funcionario', 1, NULL, NULL, NULL, 1605539068, 1605539068),
+('medico', 1, NULL, NULL, NULL, 1605539068, 1605539068),
+('SendReceita', 2, 'enviar receita', NULL, NULL, 1605539068, 1605539068),
+('updateMarcacao', 2, 'Alterar marcação', NULL, NULL, 1605539068, 1605539068),
+('UpdateMedico', 2, 'Alterar Medico', NULL, NULL, 1605539068, 1605539068),
+('utente', 1, NULL, NULL, NULL, 1605539068, 1605539068);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `auth_item_child`
+--
+
+DROP TABLE IF EXISTS `auth_item_child`;
+CREATE TABLE IF NOT EXISTS `auth_item_child` (
+  `parent` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `child` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`parent`,`child`),
+  KEY `child` (`child`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `auth_item_child`
+--
+
+INSERT INTO `auth_item_child` (`parent`, `child`) VALUES
+('admin', 'CreateMedico'),
+('admin', 'DeleteMedico'),
+('admin', 'medico'),
+('medico', 'CreateReceita'),
+('medico', 'DeleteMarcacao'),
+('medico', 'SendReceita'),
+('medico', 'updateMarcacao'),
+('medico', 'UpdateMedico'),
+('utente', 'createMarcacao'),
+('utente', 'DeleteMarcacao'),
+('utente', 'updateMarcacao');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `auth_rule`
+--
+
+DROP TABLE IF EXISTS `auth_rule`;
+CREATE TABLE IF NOT EXISTS `auth_rule` (
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `data` blob DEFAULT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  `updated_at` int(11) DEFAULT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `consultas`
+--
+
+DROP TABLE IF EXISTS `consultas`;
+CREATE TABLE IF NOT EXISTS `consultas` (
+  `id` int(11) NOT NULL,
+  `id_marcacao` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_marcacao` (`id_marcacao`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `diagnostico`
+--
+
+DROP TABLE IF EXISTS `diagnostico`;
+CREATE TABLE IF NOT EXISTS `diagnostico` (
+  `id` int(11) NOT NULL,
+  `Descri` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `situacao` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `especialidade`
+--
+
+DROP TABLE IF EXISTS `especialidade`;
+CREATE TABLE IF NOT EXISTS `especialidade` (
+  `id` int(11) NOT NULL,
+  `Name` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `especialidade`
+--
+
+INSERT INTO `especialidade` (`id`, `Name`) VALUES
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `marcacao`
+--
+
+DROP TABLE IF EXISTS `marcacao`;
+CREATE TABLE IF NOT EXISTS `marcacao` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `id_especialidade` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `migration`
+--
+
+DROP TABLE IF EXISTS `migration`;
+CREATE TABLE IF NOT EXISTS `migration` (
+  `version` varchar(180) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `migration`
+--
+
+INSERT INTO `migration` (`version`, `apply_time`) VALUES
+('m000000_000000_base', 1605538976),
+('m140506_102106_rbac_init', 1605538978),
+('m170907_052038_rbac_add_index_on_auth_assignment_user_id', 1605538978),
+('m180523_151638_rbac_updates_indexes_without_prefix', 1605538978),
+('m200409_110543_rbac_update_mssql_trigger', 1605538978),
+('m130524_201442_init', 1605539068),
+('m190124_110200_add_verification_token_column_to_user_table', 1605539068),
+('m201113_154952_init_rbac', 1605539068);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `profile`
+--
+
+DROP TABLE IF EXISTS `profile`;
+CREATE TABLE IF NOT EXISTS `profile` (
+  `id` int(11) NOT NULL,
+  `First_name` varchar(20) NOT NULL,
+  `Last_name` varchar(20) NOT NULL,
+  `Email` varchar(25) NOT NULL,
+  `Phone_number` int(11) NOT NULL,
+  `NIF` int(11) NOT NULL,
+  `Address` varchar(255) NOT NULL,
+  `Birth_date` date NOT NULL,
+  `gender` enum('Female','Male','Other','') NOT NULL,
+  `postal_code` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `NIF` (`NIF`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `receitas`
+--
+
+DROP TABLE IF EXISTS `receitas`;
+CREATE TABLE IF NOT EXISTS `receitas` (
+  `id` int(11) NOT NULL,
+  `Name` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `receitas_consultas`
+--
+
+DROP TABLE IF EXISTS `receitas_consultas`;
+CREATE TABLE IF NOT EXISTS `receitas_consultas` (
+  `id_receitas` int(11) NOT NULL,
+  `id_consultas` int(11) NOT NULL,
+  PRIMARY KEY (`id_receitas`,`id_consultas`),
+  KEY `fk_consultas` (`id_consultas`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` smallint(6) NOT NULL DEFAULT 10,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  `verification_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `password_reset_token` (`password_reset_token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `auth_assignment`
+--
+ALTER TABLE `auth_assignment`
+  ADD CONSTRAINT `auth_assignment_ibfk_1` FOREIGN KEY (`item_name`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `auth_item`
+--
+ALTER TABLE `auth_item`
+  ADD CONSTRAINT `auth_item_ibfk_1` FOREIGN KEY (`rule_name`) REFERENCES `auth_rule` (`name`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `auth_item_child`
+--
+ALTER TABLE `auth_item_child`
+  ADD CONSTRAINT `auth_item_child_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `auth_item_child_ibfk_2` FOREIGN KEY (`child`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `consultas`
+--
+ALTER TABLE `consultas`
+  ADD CONSTRAINT `fk_marcacao` FOREIGN KEY (`id_marcacao`) REFERENCES `marcacao` (`id`);
+
+--
+-- Limitadores para a tabela `receitas_consultas`
+--
+ALTER TABLE `receitas_consultas`
+  ADD CONSTRAINT `fk_consultas` FOREIGN KEY (`id_consultas`) REFERENCES `consultas` (`id`),
+  ADD CONSTRAINT `fk_receitas` FOREIGN KEY (`id_receitas`) REFERENCES `receitas` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
