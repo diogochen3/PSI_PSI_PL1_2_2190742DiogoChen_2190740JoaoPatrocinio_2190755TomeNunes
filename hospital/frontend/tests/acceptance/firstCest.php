e2b5163214626235a821aa9e9c123900d6497fee
2190742@ipleiria.pt <2190742@my.ipleiria.pt>
2020-11-18 14:54:06 +0000

    Testes de aceitação

---
+++ hospital/frontend/tests/acceptance/firstCest.php
@@ -0,0 +1,14 @@
+<?php namespace frontend\tests;
+use frontend\tests\AcceptanceTester;
+
+class firstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
---
+++ hospital/frontend/tests/unit/SigninCest.php
@@ -0,0 +1,14 @@
+<?php namespace frontend\tests\unit;
+use frontend\tests\UnitTester;
+
+class SigninCest
+{
+    public function _before(UnitTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(UnitTester $I)
+    {
+    }
+}
