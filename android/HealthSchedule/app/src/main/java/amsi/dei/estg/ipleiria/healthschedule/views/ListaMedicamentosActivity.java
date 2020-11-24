0ae592387ec816902ea7953cb6b7bc4e0e7dd795
2190742@ipleiria.pt <2190742@my.ipleiria.pt>
2020-11-24 14:57:15 +0000

    vistas

--- android/HealthSchedule/app/src/main/java/amsi/dei/estg/ipleiria/healthschedule/views/ListaMedicamentosActivity.java
+++ android/HealthSchedule/app/src/main/java/amsi/dei/estg/ipleiria/healthschedule/views/ListaMedicamentosActivity.java
@@ -5,11 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class ListaMedicamentosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_lista_medicamentos);
     }
 }
